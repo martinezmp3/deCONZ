078191bbd4ef588b452fced9d2823a6154b92902
martinezmp3 <martinez.mp3@gmail.com>
2020-09-28 15:51:36 -0400

    Update deCONZ_rest_api_Parent.groovy

--- deCONZ_rest_api_Parent.groovy
+++ deCONZ_rest_api_Parent.groovy
@@ -4,7 +4,8 @@ This driver is to control the deCONZ_rest_api from the hubitat hub.
 I wrote this diver for personal use. If you decide to use it, do it at your own risk. 
 No guarantee or liability is accepted for damages of any kind. 
         09/25/20 intial release 
-        09/29/20 add suport for motion sensor and Lights debug
+        09/26/20 add suport for motion sensor and Lights debug
+        09/27/20 add autodiscover after creation bug fix and clde cleaing  
 */
 
 
@@ -22,6 +23,7 @@ metadata {
         command "PutRequest",["string", "string"]
         command "GetConfiguration"
 //        command "webSocketStatus"
+        command "GetName"
     }
 }
 preferences {
@@ -57,13 +59,14 @@ def GetConfigurationCallBack (response, data){
         device.updateSetting("Notifyall",response.json.websocketnotifyall)
     }
 }
+
 def processCallBack(response, data) {
     if (logEnable) log.debug "processCallBack"
     if (response.hasError()){
-        log.debug "got error # ${response.getStatus()} ${response.getErrorMessage()}"
+        log.error "got error # ${response.getStatus()} ${response.getErrorMessage()}"
     }
     if (!response.hasError()){
-        log.debug "no error responce = ${response.getData()}"
+        if (logEnable) log.debug "no error responce = ${response.getData()}"
         //log.debug response.json.websocketport
     }
 }
@@ -97,6 +100,21 @@ def GetApiKey (){
 		body : ["devicetype" : "Hubitat"]
 	]
     asynchttpPost("GetApiKeyCallBack",postParams)
+}
+
+def GetName (){
+    request = "lights/4"
+    def getParams = [
+        uri: "http://${settings.ip}:${settings.port}/api/${settings.API_key}/${request}",
+        headers: ["Accept": "application/json, text/plain, */*"],
+		requestContentType: 'application/json',
+		contentType: 'application/json',
+		body : ["devicetype" : "Hubitat"]
+	]
+    response = NULL
+    asynchttpGet("processCallBack",getParams,$response)
+    log.debug "this is what i got: ${response}"
+    
 }
 def GetRequest (String request){
     if (logEnable) log.debug "GetRequest"
@@ -147,46 +165,73 @@ def discover(){
     }
 }
 
+
+def addChildCallBack (response, data){  ///[dataID: json.uniqueid]
+    if (logEnable) log.debug "addChildCallBack"
+        if (response.hasError()){
+        log.error "got error # ${response.getStatus()} ${response.getErrorMessage()}"
+    }
+    if (!response.hasError()){
+        json = response.getJson()
+        if (json.state.buttonevent){
+            if (logEnable) log.debug "no error creating Button = ${json.name}"
+            children = addChildDevice("jorge.martinez","deCONZ_rest_api_Child_Button", "child-${json.uniqueid}", [name: "Button(${json.uniqueid})", label: json.name, ID: data["dataID"], isComponent: false])
+        }
+        if (json.state.presence){
+            if (logEnable) log.debug "no error creating Motion = ${json.name}"
+            children = addChildDevice("jorge.martinez","deCONZ_rest_api_Child_Motion", "child-${json.uniqueid}", [name: "Motion(${json.uniqueid})", label: json.name, ID: data["dataID"], isComponent: false])
+        }
+        if (json.type.toString().contains("light")){
+            if (logEnable) log.debug "no error creating Ligth = ${json.name}"
+            children = addChildDevice("jorge.martinez","deCONZ_rest_api_Child_Light", "child-${json.uniqueid}", [name: "Light(${json.uniqueid})", label: json.name, ID: data["dataID"], isComponent: false])
+        }
+        if (json.type.toString().contains("plug-in")){
+            if (logEnable) log.debug "no error creating Ligth = ${json.name}"
+            children = addChildDevice("jorge.martinez","deCONZ_rest_api_Child_Light", "child-${json.uniqueid}", [name: "plug-in(${json.uniqueid})", label: json.name, ID: data["dataID"], isComponent: false])
+        }
+    }
+}
 def parse(String description) {
     if (logEnable) log.debug "parse call"
-    def json = null;
+    json = null
     try{
         json = new groovy.json.JsonSlurper().parseText(description)
+        children = getChildDevice("child-${json.uniqueid}")
         if (logEnable) log.debug "recive: ${json}"    
         if(json == null){
             log.warn "String description not parsed"
             return
         }
+        
+        if (!children && json.r != "groups"){
+            log.warn "Children NOT found creating one### ${json.r}/${json.id}"
+            def getParams = [
+            uri: "http://${settings.ip}:${settings.port}/api/${settings.API_key}/${json.r}/${json.id}",
+            headers: ["Accept": "application/json, text/plain, */*"],
+		    requestContentType: 'application/json',
+		    contentType: 'application/json',
+		    //body : ["devicetype" : "Hubitat"]
+	        ]
+            asynchttpGet("addChildCallBack",getParams, [dataID: json.id])
+            children = getChildDevice("child-${json.uniqueid}")
+        }
     }  catch(e) {
         log.error("Failed to parse json e = ${e}")
         return
     }
+//    children = getChildDevice("child-${json.uniqueid}")
     if (json.state && json.state.buttonevent){
-        def children = getChildDevice("child-${json.uniqueid}")
-        if (!children){
-            log.warn "Children NOT found creating one"
-            children = addChildDevice("jorge.martinez","deCONZ_rest_api_Child_Button", "child-${json.uniqueid}", [name: "Button(${json.uniqueid})", label: "Button(${json.uniqueid})", ID: json.id, isComponent: false])
-        }
         children.reciveData(json.state.buttonevent)
         if (logEnable) log.debug "Update for ${children.getLabel()}"
         if (json.state.lastupdated) children.updateLastUpdated(json.state.lastupdated)
     }
     if (json.state && json.state.presence!=NULL){
-        def children = getChildDevice("child-${json.uniqueid}")
-        if (!children){
-            log.warn "Children NOT found creating one"
-            children = addChildDevice("jorge.martinez","deCONZ_rest_api_Child_Motion", "child-${json.uniqueid}", [name: "Motion(${json.uniqueid})", label: "Motion(${json.uniqueid})", ID: json.id, isComponent: false])
-        }
         children.updateMotion(json.state.presence)
         if (logEnable) log.debug "Update for ${children.getLabel()} active = ${json.state.presence}"
         if (json.state.lastupdated) children.updateLastUpdated(json.state.lastupdated)
     }
     if (json.state && json.r =="lights"){
-        def children = getChildDevice("child-${json.uniqueid}")
-        if (!children){
-            log.warn "Children NOT found creating one"
-            children = addChildDevice("jorge.martinez","deCONZ_rest_api_Child_Light", "child-${json.uniqueid}", [name: "Light(${json.uniqueid})", label: "Light(${json.uniqueid})", ID: json.id , isComponent: false])
-        }
+        
         if (logEnable) log.debug "Update for ${children.getLabel()} on = ${json.state.on}"
         children.updatePower(json.state.on)
         if (json.state.bri) children.updateBri(json.state.bri)
@@ -195,7 +240,6 @@ def parse(String description) {
     }
     
     if (json.config && json.config.battery){
-        def children = getChildDevice("child-${json.uniqueid}")
         if (logEnable) log.debug "Battery Update for child-${json.uniqueid} value:${json.config.battery}"
         if (children){
             children.updateBattery(json.config.battery)
@@ -211,6 +255,7 @@ def logsOff(){
 }
 def installed() {
     if (logEnable) log.debug "installed() called"
+    discover()
     //updated()
 }
 def configure() {
